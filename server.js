91dfc9b941695ad68c725db2c376d1b28253816e
poongeta <Poonpoonpoonny@gmail.com>
2025-10-31 23:00:19 +0700

    security

--- server.js
+++ server.js
@@ -1,2 +1,47 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDB = require("./config/db");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const { xss } = require("express-xss-sanitizer");
+const rateLimit = require("express-rate-limit");
+
+dotenv.config({ path: "./config/config.env" });
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use((req, res, next) => {
+  mongoSanitize.sanitize(req.body);
+  mongoSanitize.sanitize(req.params);
+  mongoSanitize.sanitize(req.query);
+  next();
+});
+app.use(helmet());
+app.use(xss());
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, //10 mins
+  max: 100,
+});
+app.use(limiter);
+app.use(cors());
+
+
+app.set("query parser", "extended");
+
+const PORT = process.env.PORT || 5000;
+const server = app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
+
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  server.close(() => process.exit(1));
+});
+ 
+ 
